39e918696e57e1ca38a7e080d5e1dd8abe9469b4
MAntonioST <marco.teixeira4@fatec.sp.gov.br>
2025-03-05 07:29:55 -0300

    git commit -m "Add mock Pokemon list for initial development"

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+
+  pokemonList = [
+    { name: 'Bulbasaur', number: 1 },
+    { name: 'Charmander', number: 4 },
+    { name: 'Squirtle', number: 7 },
+    { name: 'Pikachu', number: 25 }
+];
+
+
 
 }
